a2a6d3b38230ac4570cb56284acee841f7e20cb6
yiyuan.zhou <yiyuan.zhou@mthreads.com>
2024-07-01 17:36:35 +0800

    modify stop.sh to stop mpirun more preciously

--- parse_megatron/stop.sh
+++ parse_megatron/stop.sh
@@ -1,1 +1,8 @@
-sudo pkill -f python
+MPIRUN_PID=$(pgrep -f "mpirun .* python parse_megatron.py")
+if [ -n "$MPIRUN_PID" ]; then
+    echo "Found MPIRUN process, PID: $MPIRUN_PID"
+    kill $MPIRUN_PID
+    echo "MPIRUN process killed."
+else
+    echo "MPIRUN process not found."
+fi

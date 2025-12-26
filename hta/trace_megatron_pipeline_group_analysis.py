c73532d7d7739381d6796edd0f69e34b27c09ea1
yongpan.hua <yongpan.hua@mthreads.com>
2025-12-26 21:41:23 +0800

    Fix: pass pp group id to set micro batch and establish_p2p_link_on_adjacent_ranks

--- hta/trace_megatron_pipeline_group_analysis.py
+++ hta/trace_megatron_pipeline_group_analysis.py
@@ -53,11 +53,11 @@ class MegatronPipelineParallelGroupTraceAnalysis(TraceAnalysis):
         logger.info('keep comm spans only')
         self.t.filter_comm_only_traces(pp_group_id=pp_group_id)
         logger.info('set_micro_batch_id')
-        self.t.set_micro_batch_id() 
+        self.t.set_micro_batch_id(pp_group_id=pp_group_id) 
         # self.t.save_traces(f'{self.output_dir}/set_micro_batch_id.json')
         
         logger.info('establish_p2p_link_on_adjacent_ranks')
-        self.t.establish_p2p_link_on_adjacent_ranks() 
+        self.t.establish_p2p_link_on_adjacent_ranks(pp_group_id=pp_group_id) 
         
         # logger.info('calculate_comm_volume_for_trace_df')
         # self.t.traces = self.t.parallel_apply(calculate_comm_volume_for_trace_df)

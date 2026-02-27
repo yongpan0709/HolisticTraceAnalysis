104cf99580d9dec8356cb1722b4672a6fda52fb3
yongpan.hua <yongpan.hua@mthreads.com>
2026-02-27 16:50:33 +0800

    Enh: pass hta_path to install_hta.sh

--- musa_examples/install_hta.sh
+++ musa_examples/install_hta.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 HOSTFILE=./hostfile
+HTA_PATH=$1
 hostlist=$(grep -v '^#\|^$' $HOSTFILE | awk '{print $1}' | xargs)
 
 for host in ${hostlist[@]}; do
   echo $host
-  ssh -f -n $host "cd /mnt/seed17/001688/huayp/1000b-train/HolisticTraceAnalysis; pip install -r requirements.txt;pip install -e ." 
+  ssh -f -n $host "cd $HTA_PATH; pip install -r requirements.txt;pip install -e ." 
   # ssh -f -n $host "apt update; apt install -y tmux"  
   echo $cmd
   ((COUNT++))
